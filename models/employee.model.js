eab93396071eba8791e88f4961d7ba146f9d51ff
bhavana-612000 <kasulabhavana.6@gmail.com>
2020-07-05 12:45:15 +0530

    First commit

---
+++ models/employee.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+var employeeSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'This field is required.'
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: Number
+    },
+    city: {
+        type: String
+    }
+});
+
+// Custom validation for email
+employeeSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+employeeSchema.path('mobile').validate((val) => {
+    mobileRegex = /^\d{10}$/ ;
+    return mobileRegex.test(val);
+}, 'Invaild number' ) ;
+    
+
+
+mongoose.model('Employee', employeeSchema);
